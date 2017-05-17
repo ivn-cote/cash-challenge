9b174af8e889ce47144fa447bbd4f38f020075fe
Ivan Kotov <kotovivan@gmail.com>
2017-05-18 01:30:05 +0200

    cleanup Home route

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,38 +1,14 @@
-/* eslint-disable react/sort-comp */
-
-import React, { PureComponent } from 'react';
-import { connect } from 'react-redux';
-// import type { Connector } from 'react-redux';
+import React from 'react';
 import Helmet from 'react-helmet';
 
-// import * as action from './action';
-// import type { Home as HomeType, Dispatch, Reducer } from '../../types';
 import styles from './styles.scss';
 
-
-// Export this for unit testing more easily
-export class Home extends PureComponent {
-  static defaultProps: {
-    home: {
-      readyStatus: 'USERS_INVALID',
-      list: null,
-    },
-    fetchUsersIfNeeded: () => {},
-  };
-
-  render() {
-    return (
-      <div className={styles.Home}>
-        <Helmet title="Home" />
-        <h3>IVN BANK</h3>
-        <p>You can get money in cash here! Please insert your card</p>
-      </div>
-    );
-  }
-}
-
-const connector = connect(
-  ({ home }) => ({ home }),
+export const Home = () => (
+  <div className={styles.Home}>
+    <Helmet title="Home" />
+    <h3>IVN BANK</h3>
+    <p>You can get money in cash here! Please insert your card</p>
+  </div>
 );
 
-export default connector(Home);
+export default Home;
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -3,11 +3,9 @@
 import { combineReducers } from 'redux';
 import { routerReducer as router } from 'react-router-redux';
 
-import home from '../containers/Home/reducer';
 import hardware from '../containers/Hardware/hardwareReducer';
 
 export default combineReducers({
-  home,
   hardware,
   router,
 });
