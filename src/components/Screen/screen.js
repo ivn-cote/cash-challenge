3187f67ac02bf6960b488975960e908474692c56
Ivan Kotov <kotovivan@gmail.com>
2017-05-19 10:10:28 +0200

    Abort buttons

--- src/components/Screen/screen.js
+++ src/components/Screen/screen.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link, Route } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
+import { urlPaths } from '../../routes';
 import styles from './screen.scss';
+
+const AbortBtn = () => (
+  <div className={styles.abortLink}>
+    <Link to={urlPaths.abort}>Get the card back</Link>
+  </div>
+);
 
 const Screen = ({ children }) => (
   <div className={styles.screen}>
     {children}
+    <Route path={urlPaths.withdrawal} component={AbortBtn} />
+    <Route path={urlPaths.pin} component={AbortBtn} />
   </div>
 );
 
