e7311dae4c4e4abab86a651900b8c53512c6945b
Ivan Kotov <kotovivan@gmail.com>
2017-05-19 17:13:13 +0200

    Back button

--- src/containers/Screen/screen.js
+++ src/containers/Screen/screen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link, Route } from 'react-router-dom';
+import { goBack } from 'react-router-redux';
 import { PropTypes } from 'prop-types';
 import { urlPaths } from '../../routes';
 import styles from './screen.scss';
@@ -11,16 +12,37 @@ const AbortBtn = () => (
   </div>
 );
 
-const Screen = ({ children, waitMode }) => (
-  <div className={styles.screen}>
-    {waitMode && <div className={styles.waitScreen} />}
-    {children}
-    <Route path={urlPaths.withdrawal} component={AbortBtn} />
-    <Route path={urlPaths.pin} component={AbortBtn} />
+const BackBtn = ({ onClick }) => (
+  <div className={styles.backLink}>
+    <a href={urlPaths.withdrawal} onClick={onClick}>Go back</a>
   </div>
 );
 
+BackBtn.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+const Screen = ({ children, waitMode, onBack }) => {
+  const goBackScreen = (evt) => {
+    evt && evt.preventDefault;
+    onBack();
+  };
+
+  return (
+    <div className={styles.screen}>
+      {waitMode && <div className={styles.waitScreen} />}
+      {children}
+      {/* should be improved */}
+      <Route path={urlPaths.withdrawalCustom} render={() => <BackBtn onClick={goBackScreen} />} />
+      <Route path={urlPaths.withdrawal} component={AbortBtn} />
+      <Route path={urlPaths.withdrawal} component={AbortBtn} />
+      <Route path={urlPaths.pin} component={AbortBtn} />
+    </div>
+  );
+};
+
 Screen.propTypes = {
+  onBack: PropTypes.func.isRequired,
   waitMode: PropTypes.bool.isRequired,
   children: PropTypes.element.isRequired,
 };
@@ -28,7 +50,11 @@ Screen.propTypes = {
 
 const connector = connect(
   ({ screen }) => ({ ...screen }),
-  () => ({}),
+  dispatch => ({
+    onBack: () => {
+      dispatch(goBack());
+    },
+  }),
 );
 
 export default connector(Screen);
